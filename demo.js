30bc6bf1e28089ba45989abec2b205e5aaebe609
Matthew Charlton <matthew.charlton@qantas.com.au>
2020-02-20 11:06:07 +1100

    add logs to demo

--- demo.js
+++ demo.js
@@ -4,6 +4,10 @@ const petApi = new PetApi();
 
 let petId = null;
 
+console.log('++++ Using Generated Code ++++')
+
+console.log('PetApi: Add a pet')
+
 petApi.addPet(
   {
     id: '9216678377732771000',
@@ -22,10 +26,12 @@ petApi.addPet(
     if (!err) {
       console.log(err, data, res.body);
       petId = res.body.id;
+      console.log('Check the pet added ok')
       console.log('petId', petId);
       petApi.getPetById(petId, (err, data, res) => {
         if (!err) {
           console.log(res.body);
+          console.log('Pet successfully added!')
         } else {
           console.log('err', err);
         }
--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const fs = require('fs');
-const process = require('child_process')
-const Converter = require('api-spec-converter');
+const fs = require("fs");
+const process = require("child_process");
+const Converter = require("api-spec-converter");
+
+console.log('++++ Swagger Client Code Gen ++++')
+
+console.log("Converting swagger file");
 
 Converter.convert({
-  from: 'swagger_2',
-  to: 'openapi_3',
-  source: './Petstore-swagger.json'
+  from: "swagger_2",
+  to: "openapi_3",
+  source: "./Petstore-swagger.json"
 }).then(function(converted) {
-  const options = { syntax: 'yaml', order: 'openapi' };
-  fs.writeFileSync('Petstore-converted.yaml', converted.stringify(options));
+  const options = { syntax: "yaml", order: "openapi" };
+  fs.writeFileSync("Petstore-converted.yaml", converted.stringify(options));
 });
 
-process.exec('npx openapi-generator generate -i Petstore-converted.yaml -g javascript -o api-services')
+console.log("Generating client code");
 
+process.exec(
+  "npx openapi-generator generate -i Petstore-converted.yaml -g javascript -o api-services"
+);
 
+console.log('++++ Swagger Client Code Gen Complete ++++ \n')
